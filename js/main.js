7b90b087bc0466cfa2d2840b082dff4903c7c265
Kin L Lim <Lim.Kin@gmail.com>
2017-09-18 15:02:25 -0400

    CSS changes

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
 
 console.log('JSLinked!');
 
-var suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+var suits = ['Spade', 'Heart', 'Club', 'Diamond'];
 var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 var deck = [];
 var players = [];
